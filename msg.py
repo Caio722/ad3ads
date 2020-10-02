118d87e7bcff7825ad718a634fdef0b36274c07e
Caio722 <caio.vinicius@aluno.faculdadeimpacta.com.br>
2020-10-02 19:45:47 -0300

    Atualizacao

--- msg.py
+++ msg.py
@@ -6,12 +6,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def nao_entre_em_panico():
+	limite = 100
+	
 
-    primos = "Tudo vai dar certo, caros alunos di ads2E, todos farao a AC em 10 minutos! "
-
-
-    return primos
+	p = 1
+	numero = 3
+	primos = "2,"
+	
+	while p < limite:
+		ehprimo = 1
+		for i in range(2, numero):
+			if numero % i == 0:
+				ehprimo = 0 
+				break
+		if (ehprimo):
+			primos = primos + str(numero) + ","
+			p += 1
+			if(p % 10 == 0):
+				primos = primos + "<br>"
+		numero += 1
+	return primos
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
